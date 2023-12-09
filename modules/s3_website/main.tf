fe62dc6480bbfa887187053fa620ea19d8166d1f
Debian <debian@vps-c0347281.vps.ovh.net>
2023-12-09 13:20:57 +0000

    quickfix

--- modules/s3_website/main.tf
+++ modules/s3_website/main.tf
@@ -57,11 +57,36 @@ resource "aws_s3_bucket_acl" "my-static-website" {
 }
 
 resource "aws_s3_object" "example" {
-  key                    = "someobject"
+  key                    = "index.html"
   bucket                 = aws_s3_bucket.my-static-website.id
-  source                 = "index.html"
+  source                 = "${path.module}/index.html"
   server_side_encryption = "AES256"
 }
+
+# S3 bucket policy
+resource "aws_s3_bucket_policy" "bucket-policy" {
+  bucket = aws_s3_bucket.my-static-website.id
+
+  policy = <<POLICY
+    {
+    "Id": "Policy",
+    "Statement": [
+        {
+        "Action": [
+            "s3:GetObject"
+        ],
+        "Effect": "Allow",
+        "Resource": "arn:aws:s3:::${aws_s3_bucket.my-static-website.bucket}/*",
+        "Principal": {
+            "AWS": [
+            "*"
+            ]
+        }
+        }
+    ]
+    }
+    POLICY
+} 
 
 # s3 static website url
 
